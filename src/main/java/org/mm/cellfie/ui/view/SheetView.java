44e786af8e1ec090a1e54222a9b7a72ce75e0b2b
johardi <josef.hardi@stanford.edu>
2015-09-07 12:59:57 -0700

    Move the sheet table model as inner class.

--- src/main/java/org/mm/cellfie/ui/view/SheetView.java
+++ src/main/java/org/mm/cellfie/ui/view/SheetView.java
@@ -5,9 +5,11 @@ import java.awt.BorderLayout;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
+import javax.swing.table.AbstractTableModel;
 
+import org.apache.poi.ss.usermodel.Cell;
 import org.apache.poi.ss.usermodel.Sheet;
-import org.mm.cellfie.ui.model.SheetModel;
+import org.mm.ss.SpreadSheetUtil;
 import org.mm.ui.ModelView;
 
 public class SheetView extends JPanel implements ModelView
@@ -15,12 +17,12 @@ public class SheetView extends JPanel implements ModelView
 	private static final long serialVersionUID = 1L;
 
 	private final Sheet sheet;
-	private final SheetModel sheetModel;
+	private final SheetTableModel sheetModel;
 
 	public SheetView(Sheet sheet)
 	{
 		this.sheet = sheet;
-		sheetModel = new SheetModel(sheet);
+		sheetModel = new SheetTableModel(sheet);
 
 		setLayout(new BorderLayout());
 
@@ -45,5 +47,67 @@ public class SheetView extends JPanel implements ModelView
 	public void update()
 	{
 		// NO-OP
+	}
+	
+	class SheetTableModel extends AbstractTableModel
+	{
+		private static final long serialVersionUID = 1L;
+
+		private final Sheet sheet;
+
+		public SheetTableModel(Sheet sheet)
+		{
+			this.sheet = sheet;
+		}
+
+		public int getRowCount()
+		{
+			return sheet.getLastRowNum() + 1;
+		}
+
+		public int getColumnCount()
+		{
+			int maxCount = 0;
+			for (int i = 0; i < getRowCount(); i++) {
+				int currentCount = sheet.getRow(i).getLastCellNum();
+				if (currentCount > maxCount) {
+					maxCount = currentCount;
+				}
+			}
+			return maxCount;
+		}
+
+		public String getColumnName(int column)
+		{
+			return SpreadSheetUtil.columnNumber2Name(column+1);
+		}
+
+		public Object getValueAt(int row, int column)
+		{
+			try {
+				Cell cell = sheet.getRow(row).getCell(column);
+				switch (cell.getCellType()) {
+					case Cell.CELL_TYPE_BLANK: return "";
+					case Cell.CELL_TYPE_STRING: return cell.getStringCellValue();
+					case Cell.CELL_TYPE_NUMERIC: 
+						if (isInteger(cell.getNumericCellValue())) { // check if the numeric is an integer or double
+							return (int) cell.getNumericCellValue();
+						} else {
+							return cell.getNumericCellValue();
+						}
+					case Cell.CELL_TYPE_BOOLEAN: return cell.getBooleanCellValue();
+					case Cell.CELL_TYPE_FORMULA: return cell.getNumericCellValue();
+					default: return "";
+				}
+			} catch (NullPointerException e) {
+				// TODO Log this strange error
+				return "";
+			}
+		}
+
+		private boolean isInteger(double number)
+		{
+			return (number == Math.floor(number) && !Double.isInfinite(number));
+		}
 	}
 }
