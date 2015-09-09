3f73551622d644cf0eb2960e70fee4f1685d39b8
johardi <josef.hardi@stanford.edu>
2015-09-09 15:16:07 -0700

    Create CellfieException to handle all exceptions in the plugin.

---
+++ src/main/java/org/mm/cellfie/ui/exception/CellfieException.java
@@ -0,0 +1,16 @@
+package org.mm.cellfie.ui.exception;
+
+public class CellfieException extends Exception
+{
+	private static final long serialVersionUID = 1L;
+
+	public CellfieException(String message)
+	{
+		super(message);
+	}
+
+	public CellfieException(String message, Throwable cause)
+	{
+		super(message, cause);
+	}
+}
