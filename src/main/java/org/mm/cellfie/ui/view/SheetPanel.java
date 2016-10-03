3c75704d2cb67e4d3f009d3f7a01fc0de3c69156
johardi <josef.hardi@stanford.edu>
2016-10-03 14:28:25 -0700

    Closes #92

--- src/main/java/org/mm/cellfie/ui/view/SheetPanel.java
+++ src/main/java/org/mm/cellfie/ui/view/SheetPanel.java
@@ -187,7 +187,7 @@ public class SheetPanel extends JPanel {
       private void selectCellRangeOnMouseDragged() {
          int[] selectedColumns = tblBaseSheet.getSelectedColumns();
          int[] selectedRows = tblBaseSheet.getSelectedRows();
-         if (selectedColumns.length == 0 && selectedRows.length == 0) {
+         if (selectedColumns.length == 0 || selectedRows.length == 0) {
             setSelectionRange(START_INDEX, START_INDEX, START_INDEX, END_INDEX); // A:A
          } else {
             setSelectionRange(selectedColumns[0], selectedRows[0],
